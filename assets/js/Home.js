bea8e29fb85410a46acf22f42f7b77269b42e395
quocdat233 <datvq.24it@vku.udn.vn>
2025-04-14 18:42:28 +0700

    mhehe

--- assets/js/Home.js
+++ assets/js/Home.js
@@ -71,7 +71,7 @@ const teamSwiper = new Swiper(".teamSwiper", {
   spaceBetween: 20,
   loop: true,
   autoplay: {
-    delay: 3500,
+    delay: 3000,
     disableOnInteraction: false,
   },
   grabCursor: true,
